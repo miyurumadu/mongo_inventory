8375d07a8ebf9f0b2d7191792bd5d89eb5070e60
miyurumadu <miyurumadusanka@yahoo.com>
2017-07-15 21:39:48 +0530

    ordered the files successfully

--- src/main/java/com/thogakade/inventory/PlaceOrder.java
+++ src/main/java/com/thogakade/inventory/PlaceOrder.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.thogakade.db;
+package com.thogakade.inventory;
 
 import com.thogakade.inventory.*;
 import com.thogakade.controller.CustomerController;
